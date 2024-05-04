24d217537eca9fbce7daa4ff6980d7a51e623186
fred <fredpret@protonmail.ch>
2024-05-04 08:09:15 -0600

    switching to Quill.js

--- cms/models.py
+++ cms/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from ckeditor.fields import RichTextField
+#from quill.fields import RichTextField
 
 # Create your models here.
 from django.contrib.auth.models import User
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 class Article(models.Model):
     title = models.TextField()
-    content = RichTextField()
+    #content = RichTextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     published = models.BooleanField(default=False)
